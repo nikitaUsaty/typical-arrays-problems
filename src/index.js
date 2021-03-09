dd20d09774d615f4c2d01f9bddc897c6e316a29f
nikitausaty <nikitausatiy@yandex.ru>
2021-03-09 20:03:08 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+exports.min = function min(array) {
+    if (array && array.length) {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array && array.length) {
+        return Math.max(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array && array.length) {
+        let reducer = array.reduce((a, b) => a + b);
 
-exports.avg = function avg (array) {
-  return 0;
-}
+        return reducer / array.length;
+    } else {
+        return 0;
+    }
+};
